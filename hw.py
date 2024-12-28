0133dafa82a12166ae3f6f96bf6141a6bfe02656
sruthy047 <79702452+sruthy047@users.noreply.github.com>
2024-12-29 00:32:30 +0530

    Add files via upload

--- hw.py
+++ hw.py
@@ -1,28 +1,58 @@
 import cv2
-import os
-img=cv2.imread("E:/Sruthi/Open CV/Lesson 1/h1.png",cv2.IMREAD_COLOR)
-cv2.imshow("Cherry Image",img)#colored image
-cv2.waitKey(0)
-#grayscale image
-img1=cv2.imread("E:/Sruthi/Open CV/Lesson 1/h1.png",0)
-cv2.imshow("Cherry Image in Gray Scale",img1)
-cv2.waitKey(0)
-#saving image
-saved_directory="E:\Sruthi\Open CV\Lesson 1\saved"
-os.chdir(saved_directory)
-cv2.imwrite("cherry.png",img1)
+import numpy as np
+import time
+print(cv2.__version__)
 
-print("Successfully saved")
-# Split the ah1ove image in red, h1lue and green different saturations
-img2=cv2.imread("E:/Sruthi/Open CV/Lesson 1/h1.png",1)
-cv2.imshow("Original image",img2)
-cv2.waitKey(0)
-h1,G,R =cv2.split(img2)
+raw_video=cv2.VideoCapture("E:\Sruthi\Open CV\Lesson 7\video.mp4")
 
-cv2.imshow("h1lue Saturation Image",h1)
-cv2.waitKey(0)
-cv2.imshow("Green Saturation Image",G)
-cv2.waitKey(0)
-cv2.imshow("Red Saturation Image",R)
-cv2.waitKey(0)
+time.sleep(1)
+count=0
+background=None
+
+for i in range(60):
+    return_val,background=raw_video.read()
+    if return_val == False:
+        continue
+    #
+    
+if background is None:
+    print("Error")
+    raw_video.release()
+    exit()
+background=np.flip(background,axis=1)
+
+while(raw_video.isOpened()):
+    return_val,img=raw_video.read()
+    if not return_val:
+        break
+    count+=1
+    img=np.flip(img,axis=1)
+
+    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+    
+    lower_green = np.array([35, 40, 40])  # Lower bound for green in HSV
+    upper_green = np.array([85, 255, 255]) 
+
+    mask1=cv2.inRange(hsv,lower_green,upper_green)
+
+    lower_green=([90,40,40])
+    upper_green=([180,255,255])
+
+    mask2=cv2.inRange(hsv,lower_green,upper_green)
+    mask1=mask1+mask2
+
+    mask1=cv2.morphologyEx(mask1,cv2.MORPH_OPEN,np.ones((3,3),np.unit8),iterations=1)
+    mask2=cv2.bitwise_not(mask1)
+
+    res1 = cv2.bitwise_and(background, background, mask = mask1)
+    res2 = cv2.bitwise_and(img, img, mask = mask2)
+    final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
+
+    cv2.imshow("INVISIBLE MAN", final_output)
+    k = cv2.waitKey(10)
+    if k == 27:
+        break
+
+raw_video.release()
 cv2.destroyAllWindows()
